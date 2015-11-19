c12e2928dd125631b982034c30d4e335a1efba2e
David Marczinus <david.marczinus@gmail.com>
2015-11-19 12:39:26 +0100

    Added .gitconfig, some new stuff added to setup

--- vm-setup.sh
+++ vm-setup.sh
@@ -5,28 +5,35 @@ cd setup-temp
 sudo add-apt-repository -y ppa:webupd8team/sublime-text-3
 sudo add-apt-repository -y ppa:webupd8team/java
 sudo add-apt-repository -y ppa:numix/ppa
-wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add - 
-sudo sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
+sudo apt-add-repository -y ppa:zeal-developers/ppa
+sudo add-apt-repository -y ppa:fkrull/deadsnakes
+
+#wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add - 
+#sudo sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
 
 # Sun JDK setup
 echo oracle-java8-installer shared/accepted-oracle-license-v1-1 select true | sudo /usr/bin/debconf-set-selections
 sudo apt-get install -y oracle-java8-set-default
 
 # App setup
-sudo apt-get install -y synaptic zsh git curl sublime-text-installer xclip google-chrome-stable numix-gtk-theme
+sudo apt-get install -y synaptic zsh git curl sublime-text-installer xclip google-chrome-stable numix-gtk-theme vlc zeal python3.5
 
-# Install oh-my-zsh, set ZSH as default shell, set theme to solarized
+# Set sublime text as default editor
+cp /usr/share/applications/defaults.list ~/.defaults.list.bak
+sed -i 's/gedit/subl/' /usr/share/applications/defaults.list
+
+# Install oh-my-zsh, set ZSH as default shell
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-cp ~/.zshrc ~/.zshrc
-rm ~/.zshrc
-
-curl -fsSL https://raw.githubusercontent.com/marczinusd/dev-setup/master/.zshrc > ~/.zshrc
-
 chsh -s /bin/zsh
 
-curl -fsSL https://github.com/powerline/fonts/raw/master/Inconsolata-g/Inconsolata-g%20for%20Powerline.otf > inconsolata.otf
+cp ~/.zshrc ~/.zshrc
+rm ~/.zshrc
+curl -fsSL https://raw.githubusercontent.com/marczinusd/dev-setup/master/.zshrc > ~/.zshrc
+
+curl -fsSL https://github.com/powerline/fonts/raw/master/Inconsolata-g/Inconsolata-g%20for%20Powerline.otf > inconsolata.otf #set this manually later
 sudo cp inconsolata.otf /usr/share/fonts
 
+# solarized
 wget --no-check-certificate https://raw.github.com/seebi/dircolors-solarized/master/dircolors.ansi-dark
 mv dircolors.ansi-dark .dircolors
 eval `dircolors ~/.dircolors`
@@ -40,6 +47,12 @@ git config --global user.name "David Marczinus"
 git config --global user.email david.marczinus@gmail.com
 git config --global core.editor subl
 
+# Node setup
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.29.0/install.sh | zsh
+source ~/.zshrc
+nvm install 5.1.0
+nvm alias default 5.1.0
+
 # ATTENDED FROM HERE
 # Set up SSH key for GitHub (attended)
 ssh-keygen -t rsa -b 4096 -C "marczinus.david@gmail.com"
@@ -52,5 +65,8 @@ sensible-browser https://github.com/settings/ssh
 cd ~
 wget -O /tmp/intellij.tar.gz http://download.jetbrains.com/idea/ideaIU-15.0.1.tar.gz && tar xfz /tmp/intellij.tar.gz && cd idea*/bin && ./idea.sh
 
+#update OS
+sudo apt-get update && sudo apt-get -y upgrade 
 
-
+# set p4merge as git diff tool
+# http://blogs.pdmlab.com/alexander.zeitler/articles/installing-and-configuring-p4merge-for-git-on-ubuntu/
