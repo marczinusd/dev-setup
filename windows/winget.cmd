e95b1892545695520f6169a7c432fec052cfaf8f
David Marczinus <marczinus.david@gmail.com>
2021-09-08 19:32:26 +0200

    chore: update setup scripts and linux configs

---
+++ windows/winget.cmd
@@ -0,0 +1,62 @@
+@REM Enable winget: https://docs.microsoft.com/en-us/windows/package-manager/winget/
+@REM Browser(s)
+winget install Mozilla.FirefoxDeveloperEdition
+@REM winget install Google.Chrome.Beta @REM pls no
+
+@REM Development
+winget install Microsoft.dotnet
+@REM winget install Microsoft.dotnetPreview @REM Sometimes useful :)
+winget install JetBrains.Rider
+winget install Microsoft.VisualStudioCode
+winget install Git.Git
+winget install Fork.Fork
+winget install Docker.DockerDesktop
+winget install Microsoft.WindowsTerminalPreview
+winget install Insomnia.Insomnia
+winget install PostgreSQL.pgAdmin
+winget install JanDeDobbeleer.OhMyPosh
+winget install bmatzelle.Gow
+winget install gsudo
+winget install JackieLiu.NotepadsApp
+
+@REM winget install SublimeHQ.SublimeMerge
+winget install Fork.Fork
+
+@REM Communication
+winget install Discord.Discord
+winget install Krisp.Krisp
+@REM winget install Zoom.Zoom
+
+@REM Media
+winget install Streamlink.Streamlink
+winget install Streamlink.StreamlinkTwitchGui
+winget install Spotify.Spotify
+winget install Daum.Potplayer
+
+@REM Games
+winget install Valve.Steam
+winget install EpicGames.EpicGamesLauncher
+winget install GOG.Galaxy
+winget install Codeusa.BorderlessGaming
+
+@REM Misc
+winget install Citrix.WorkspaceApp
+@REM winget install OliverSchwendener.ueli
+winget install Microsoft.PowerToys
+winget install qBittorrent.qBittorrent
+winget install calibre.calibre
+winget install JAMSoftware.TreeSizeFree
+winget install 7zip.7zip
+winget install QL-Win.QuickLook
+winget install Ghisler.TotalCommander
+winget install File-New-Project.EarTrumpet
+winget install Joplin.Joplin
+winget install Files-Community.Files
+@REM winget install BinaryFortress.DisplayFusion @REM Might not work with steam license?
+
+@REM Missing stuff
+@REM WSL2 + Pengwin: https://docs.microsoft.com/en-us/windows/wsl/install-win10
+@REM TaskbarX: https://chrisandriessen.nl/taskbarx
+@REM Caffeine: https://www.zhornsoftware.co.uk/caffeine/
+@REM X410: https://x410.dev/
+@REM DisplayFusion (steam): https://www.displayfusion.com/
